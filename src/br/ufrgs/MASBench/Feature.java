7394a52776216f60ae243c55f17aabab099e4b66
sim <phd.abel@gmail.com>
2015-09-01 11:53:03 -0300

    entities, features, and instances

--- src/br/ufrgs/MASBench/Feature.java
+++ src/br/ufrgs/MASBench/Feature.java
@@ -1,11 +1,44 @@
 package br.ufrgs.MASBench;
 
+import java.util.HashMap;
+
 public class Feature extends Entity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5635483259848026695L;
+	
+	//HashMap<instance,entityType>
+	private HashMap<Entity, Integer> instanceList;
+	
+	public Feature()
+	{
+		super();
+		this.instanceList = new HashMap<>();
+	}
+	
+	public Feature(int id)
+	{
+		super(id);
+		this.instanceList = new HashMap<Entity,Integer>();
+	}
+	
+	public Feature(int id, int dimension)
+	{
+		super(id, dimension);
+		this.instanceList = new HashMap<Entity,Integer>();
+	}
+	
+	public void addInstance(Entity element, Integer entityType)
+	{
+		this.instanceList.put(element, entityType);
+	}
+	
+	public HashMap<Entity, Integer> getInstanceList()
+	{
+		return this.instanceList;
+	}
 	
 	
 
